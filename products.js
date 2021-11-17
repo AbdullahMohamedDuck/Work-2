84188dac6bd43dd61bb2fa2b170a5719c8e2d0d5
Abdullah <67029870+spades96@users.noreply.github.com>
2021-11-17 19:26:32 +0000

    Main foundation

---
+++ products.js
@@ -0,0 +1,96 @@
+let products = [
+      
+{  
+    "id":1001,
+    "title":"Subject: Maths",
+    "description":"Learn maths with this subject",
+    "price":99,
+    "location": "London",
+    "image":"images/maths.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1002,
+    "title":"Subject: Art",
+    "description":"Get creative with Art classes",
+    "price": 100,
+    "location": "London",
+    "image":"images/art.jpg", 
+    "availableInventory":5     
+},
+
+{  
+    "id":1003,
+    "title":"Subject: Biology",
+    "description":"Discover the human body!",
+    "price": 150,
+    "location": "London",
+    "image":"images/biology.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1004,
+    "title":"Subject: Chemistry",
+    "description":"Learn chemistry with this subject",
+    "price": 200,
+    "location": "Liverpool",
+    "image":"images/chemistry.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1005,
+    "title":"Subject: Computing",
+    "description":"Learn to code with computing classes",
+    "price":129,
+    "location": "York",
+    "image":"images/computing.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1006,
+    "title":"Subject: Engineering",
+    "description":"Learn the fundamentals of design and building",
+    "price":200,
+    "location": "Manchester",
+    "image":"images/engineering.jpg", 
+    "availableInventory":5
+},
+{  
+    "id":1007,
+    "title":"Subject: English",
+    "description":"Learn English with this subject",
+    "price":99,
+    "location": "Cambridge",
+    "image":"images/english.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1008,
+    "title":"Subject: Gym",
+    "description":"Get fit with Gym classes!!",
+    "price":40,
+    "location": "London",
+    "image":"images/gym.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1009,
+    "title":"Subject: Music",
+    "description":"Learn to play instruments",
+    "price":99,
+    "location": "Sheffield",
+    "image":"images/music.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1010,
+    "title":"Subject: Physics",
+    "description":"Learn Physics with this subject",
+    "price":200,
+    "location": "Bristol",
+    "image":"images/physics.jpg", 
+    "availableInventory":5     
+}
+
+
+]
